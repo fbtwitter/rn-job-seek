359e59bc2d1d67036043efcd8d272584d7c0826d
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2023-12-02 22:44:00 +0700

    adding required assets and constants

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
-import { View,  Text} from 'react-native'
+import { View, Text, ScrollView, SafeAreaView } from 'react-native'
+import { useState } from 'react'
+import { Stack, useRouter } from 'expo-router'
+
+import { COLORS, icons, images, SIZES } from '../constans'
 
 const Home = () => {
+  const router = useRouter()
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Home New</Text>
+    </SafeAreaView>
   )
 }
 
